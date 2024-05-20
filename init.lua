8b244ebe11fbc533360440df3b9619903c14dc45
Daniel Walter <daniel01walter@gmail.com>
2024-05-20 17:33:49 +0200

    test push

--- init.lua
+++ init.lua
@@ -28,7 +28,7 @@ vim.opt.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
-
+-- test to push
 -- Enable break indent
 vim.opt.breakindent = true
 
